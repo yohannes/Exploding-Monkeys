c945649b851be25480b012a47149ecc9c1a74bff
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-10-21 16:12:51 +0700

    Set up the background color and method to create array of buildings.

--- Exploding Monkeys/GameScene.swift
+++ Exploding Monkeys/GameScene.swift
@@ -9,8 +9,16 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
     
+    // MARK: - Stored Properties
+    
+    var buildings = Array<BuildingNode>()
+    
+    // MARK: - Methods Override
+    
+    override func didMoveToView(view: SKView) {
+        self.backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        self.createBuildings()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -19,5 +27,23 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    // MARK: - Local Methods
+    
+    func createBuildings() {
+        var currentX: CGFloat = -15
+        
+        while currentX < 1024 {
+            let size = CGSize(width: RandomInt(2, max: 4) * 40, height: RandomInt(300, max: 600))
+            currentX += size.width + 2
+            
+            let building = BuildingNode(color: UIColor.redColor(), size: size)
+            building.position = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+            building.setup()
+            self.addChild(building)
+            
+            self.buildings.append(building)
+        }
     }
 }
